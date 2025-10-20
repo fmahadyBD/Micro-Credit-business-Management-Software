274ab7cdccec00f7f4dea2ae1ee844ef12bac0d8
fmahadyBD <fmahady01@gmail.com>
2025-10-20 14:02:57 +0600

    generate the components

---
+++ frontend/src/app/shared/components/data-tables/data-tables.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-tables',
+  imports: [],
+  templateUrl: './data-tables.component.html',
+  styleUrl: './data-tables.component.css'
+})
+export class DataTablesComponent {
+
+}
